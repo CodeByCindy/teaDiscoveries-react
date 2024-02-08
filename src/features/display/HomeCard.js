bf0a726476ef07c81c3f113b79355e8f72431805
Huang <cindy.psu.h@gmail.com>
2024-02-07 16:08:59 -0800

    image adjust

--- src/features/display/HomeCard.js
+++ src/features/display/HomeCard.js
@@ -5,17 +5,17 @@ import homeCardImage from '../../app/assets/images/teaPlanting.jpg';
 const HomeCard = () => {
     return (
         <Card className="w-100">
-            <CardImg width="100%" src={homeCardImage} alt='home-card-image'/>
+            <CardImg src={homeCardImage} alt='home-card-image' />
             {/* <CardBody>
                 <CardText>{description}</CardText>
             </CardBody> */}
-              <CardImgOverlay>
-              <CardBody style={{ color: 'white', fontSize: '60px', textAlign: 'center' , fontWeight:'bold', paddingTop: '200px'}}>
-              Tea Adventure
-            </CardBody>
-              <CardText style={{ color: 'white', fontSize: '40px', textAlign: 'center' , fontWeight:'bold', paddingTop: '20px'}}>
-              Explore, and Cultivate Your Perfect Cup!
-            </CardText>
+            <CardImgOverlay>
+                <CardBody style={{ color: 'white', fontSize: '60px', textAlign: 'center' , fontWeight:'bold', paddingTop: '200px'}}>
+                Tea Adventure
+                </CardBody>
+                  <CardText style={{ color: 'white', fontSize: '40px', textAlign: 'center' , fontWeight:'bold', paddingTop: '20px'}}>
+                  Explore, and Cultivate Your Perfect Cup!
+                </CardText>
               </CardImgOverlay>
         </Card>
     );
